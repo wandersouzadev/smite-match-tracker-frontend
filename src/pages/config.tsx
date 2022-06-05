78ce8ec87f40a7cae13c70e57b375e718b2a760f
Wander Souza <wandersouza@live.com>
2022-06-05 17:14:22 -0300

    change folder name, app to pages

--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
+import "../styles/global.scss";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
