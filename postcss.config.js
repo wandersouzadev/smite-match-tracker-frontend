0630c4dee00df0df2058b0b559a321f05fcb5637
Wander Souza <wandersouza@live.com>
2022-06-05 17:11:20 -0300

    add postcss-plugin px to rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [require('postcss-preset-env')]
+  plugins: [require('postcss-preset-env'), 
+  require('postcss-pxtorem')({
+    rootValue: 16,
+    propList: ['*'],
+    exclude: /node_modules/i
+  })]
 }
