30dd8a962fa782edca023ddb21bd746a714fb723
Wander Souza <wandersouza@live.com>
2022-06-05 17:20:29 -0300

    change configuration template to use sass modules

--- src/template/config/config.tsx
+++ src/template/config/config.tsx
@@ -1,23 +1,33 @@
 import { AccountList } from "@/components/account-list";
 import { SmiteSearchConfirmation } from "@/components/smite-search-confirmation";
 import { TwitchAccount } from "@/components/twitch-account";
-import { useTwitchHelper } from "@/hooks/use-twitch-helper";
+import { useTwitchAuth } from "@/hooks/use-twitch-auth";
+import { TwitterLogo } from "phosphor-react";
 import React from "react";
-import * as S from "./styles";
+import Styles from "./styles.module.scss";
 
 export const ConfigTemplate: React.FC = () => {
-  useTwitchHelper();
+  useTwitchAuth();
   return (
-    <S.ConfigPageWrapper>
-      <S.Aside>
-        <div />
+    <div className={Styles["config-wrapper"]}>
+      <aside className={Styles.aside}>
         <AccountList />
         <TwitchAccount />
-      </S.Aside>
-      <S.Main>
+      </aside>
+      <main className={Styles.main}>
         <img src="logo.png" alt="logo" width={400} height={100} />
         <SmiteSearchConfirmation />
-      </S.Main>
-    </S.ConfigPageWrapper>
+      </main>
+      <footer className={Styles.footer}>
+        <a
+          href="https://twitter.com/wandersouzadev"
+          target="__blank"
+          rel="noreferrer"
+        >
+          <TwitterLogo size={32} />
+        </a>
+        <b>Wander Souza</b>
+      </footer>
+    </div>
   );
 };
