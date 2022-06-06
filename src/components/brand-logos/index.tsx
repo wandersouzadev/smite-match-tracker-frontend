25e53b37404615d2aaa14d06b262c45c82732525
Wander Souza <wandersouza@live.com>
2022-06-05 23:01:00 -0300

    update platform icon component

--- src/components/brand-logos/index.tsx
+++
@@ -1,1 +0,0 @@
-export { BrandLogos } from "./brand-logos";
---
+++ src/components/platform-icon/platform-icon.tsx
@@ -0,0 +1,34 @@
+import { ReactComponent as EpicLogoSvg } from "@/assets/epic-logo.svg";
+import { ReactComponent as HirezLogoSvg } from "@/assets/hirez-logo.svg";
+import { ReactComponent as NintendoSwitchLogoSvg } from "@/assets/nintendo-switch-logo.svg";
+import { ReactComponent as PlaystationLogoSvg } from "@/assets/playstation-logo.svg";
+import { ReactComponent as SteamLogoSvg } from "@/assets/steam-logo.svg";
+import { ReactComponent as XboxLogoSvg } from "@/assets/xbox-logo.svg";
+import React from "react";
+
+interface Props {
+  platformName: string;
+  width?: number;
+  height?: number;
+}
+
+export const PlatformIcon: React.FC<Props> = ({
+  platformName,
+  width = 24,
+  height = 24
+}) => {
+  switch (platformName) {
+    case "Steam":
+      return <SteamLogoSvg width={width} height={height} />;
+    case "Epic Games":
+      return <EpicLogoSvg width={width} height={height} />;
+    case "Switch":
+      return <NintendoSwitchLogoSvg width={width} height={height} />;
+    case "PSN":
+      return <PlaystationLogoSvg width={width} height={height} />;
+    case "XboxLive":
+      return <XboxLogoSvg width={width} height={height} />;
+    default:
+      return <HirezLogoSvg width={width} height={height} />;
+  }
+};
