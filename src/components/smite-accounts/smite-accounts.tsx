0a873efd2881c0b02880171e765912928657649f
Wander Souza <wandersouza@live.com>
2022-06-05 23:15:24 -0300

    refactor display smite accounts in config page

--- src/components/smite-accounts/smite-accounts.tsx
+++ src/components/smite-accounts/smite-accounts.tsx
@@ -1,13 +1,12 @@
-import { ReactComponent as CheckIcon2 } from "@/assets/check2-icon.svg";
-import { ReactComponent as TrashIcon } from "@/assets/trash-icon.svg";
-import { BrandLogos } from "@/components/brand-logos";
+import { PlatformIcon } from "@/components/platform-icon";
 import { useEbs } from "@/hooks/use-ebs";
 import { smiteAccountState } from "@/recoil/atoms/smite-context-account";
 import { twitchAuthDataState } from "@/recoil/atoms/twitch-auth-data";
 import { twitchHelperState } from "@/recoil/atoms/twitch-helper";
+import { ArrowUp, Trash } from "phosphor-react";
 import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import * as S from "./styles";
+import Styles from "./styles.module.scss";
 
 export const SmiteAccounts: React.FC = () => {
   const twitchHelper = useRecoilValue(twitchHelperState);
@@ -29,16 +28,16 @@ export const SmiteAccounts: React.FC = () => {
     }
   }, [data, setSmiteAccounts]);
 
-  const handleChangeMainAccount = (i: number) => {
+  const handleChangeMainAccount = (accountIndex: number) => {
     if (!twitchHelper || !smiteAccounts) {
       return;
     }
 
     const filteredAccounts = smiteAccounts.filter(
-      (accounts, index) => index !== i
+      (accounts, index) => index !== accountIndex
     );
 
-    filteredAccounts.unshift(smiteAccounts[i]);
+    filteredAccounts.unshift(smiteAccounts[accountIndex]);
 
     twitchHelper.configuration.set(
       "broadcaster",
@@ -73,39 +72,40 @@ export const SmiteAccounts: React.FC = () => {
     return <h1>Loading...</h1>;
   }
   if (isError) {
-    return <h1>error</h1>;
+    return <h1>Error</h1>;
   }
 
   return (
-    <S.AccountsWrapper>
-      <S.Title>MY ACCOUNTS</S.Title>
-      <S.Accounts>
+    <div className={Styles.wrapper}>
+      <ul className={Styles["account-list"]}>
         {(smiteAccounts?.length &&
           smiteAccounts?.map((account, accountIndex) => (
-            <S.Account key={account.Id}>
-              <S.AccountWrapper>
-                <S.AccountInfo>
-                  <BrandLogos iconName={account.Platform!} />
-                  {account.hz_player_name}
-                </S.AccountInfo>
-                {accountIndex ? (
-                  <S.AccountActions>
-                    <S.ActionButton
-                      onClick={() => handleChangeMainAccount(accountIndex)}
-                    >
-                      <CheckIcon2 />
-                    </S.ActionButton>
-                    <S.ActionButton
-                      onClick={() => handleRemoveSmiteAccount(accountIndex)}
-                    >
-                      <TrashIcon />
-                    </S.ActionButton>
-                  </S.AccountActions>
-                ) : null}
-              </S.AccountWrapper>
-            </S.Account>
-          ))) || <strong>no accounts</strong>}
-      </S.Accounts>
-    </S.AccountsWrapper>
+            <li className={Styles.account} key={account.Id}>
+              <div className={Styles["account-wrapper"]}>
+                <PlatformIcon platformName={account.Platform!} />
+                <p>{account.hz_player_name}</p>
+
+                <div className={Styles["account-actions"]}>
+                  <button
+                    disabled={!accountIndex}
+                    style={accountIndex ? {} : { color: "transparent" }}
+                    type="button"
+                    onClick={() => handleChangeMainAccount(accountIndex)}
+                  >
+                    <ArrowUp size={24} weight="fill" />
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveSmiteAccount(accountIndex)}
+                  >
+                    <Trash size={24} weight="fill" />
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))) || <template />}
+      </ul>
+    </div>
   );
 };
--- src/template/config/config.tsx
+++ src/template/config/config.tsx
@@ -2,23 +2,30 @@ import { SmiteAccounts } from "@/components/smite-accounts";
 import { SmiteSearchConfirmation } from "@/components/smite-search-confirmation";
 import { TwitchProfile } from "@/components/twitch-profile";
 import { useTwitchAuth } from "@/hooks/use-twitch-auth";
-import { TwitterLogo } from "phosphor-react";
+import { GithubLogo, TwitterLogo } from "phosphor-react";
 import React from "react";
 import Styles from "./styles.module.scss";
 
 export const ConfigTemplate: React.FC = () => {
   useTwitchAuth();
+
   return (
     <div className={Styles["config-wrapper"]}>
       <aside className={Styles.aside}>
-        <SmiteAccounts />
-        <TwitchProfile />
+        <div className={Styles["smite-accounts"]}>
+          <h2>My Accounts</h2>
+          <SmiteAccounts />
+        </div>
+        <div className={Styles["twitch-profile"]}>
+          <TwitchProfile />
+        </div>
       </aside>
       <main className={Styles.main}>
         <img src="logo.png" alt="logo" width={400} height={100} />
         <SmiteSearchConfirmation />
       </main>
       <footer className={Styles.footer}>
+        <b>Smite Match Tracker v0.1.0</b>
         <a
           href="https://twitter.com/wandersouzadev"
           target="__blank"
@@ -26,7 +33,13 @@ export const ConfigTemplate: React.FC = () => {
         >
           <TwitterLogo size={32} />
         </a>
-        <b>Wander Souza</b>
+        <a
+          href="https://github.com/wandersouzadev"
+          target="__blank"
+          rel="noreferrer"
+        >
+          <GithubLogo size={32} />
+        </a>
       </footer>
     </div>
   );
