f78b22b239100e164f564dd0c3e17dfdb3c7bc8c
Wander Souza <wandersouza@live.com>
2022-06-08 13:50:48 -0300

    add PC icon to platform icon component

--- src/components/platform-icon/platform-icon.tsx
+++ src/components/platform-icon/platform-icon.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as EpicLogoSvg } from "@/assets/epic-logo.svg";
 import { ReactComponent as HirezLogoSvg } from "@/assets/hirez-logo.svg";
 import { ReactComponent as NintendoSwitchLogoSvg } from "@/assets/nintendo-switch-logo.svg";
+import { ReactComponent as PClogoSvg } from "@/assets/pc-logo.svg";
 import { ReactComponent as PlaystationLogoSvg } from "@/assets/playstation-logo.svg";
 import { ReactComponent as SteamLogoSvg } from "@/assets/steam-logo.svg";
 import { ReactComponent as XboxLogoSvg } from "@/assets/xbox-logo.svg";
@@ -18,6 +19,8 @@ export const PlatformIcon: React.FC<Props> = ({
   height = 24
 }) => {
   switch (platformName) {
+    case "PC":
+      return <PClogoSvg width={width} height={height} />;
     case "Steam":
       return <SteamLogoSvg width={width} height={height} />;
     case "Epic Games":
