08fe757bbf448d3a3fb39de7a968fa549f17c7cf
Wander Souza <wandersouza@live.com>
2022-06-17 19:22:54 -0300

    add twitch context atom

--- src/recoil/atoms/smite-context-account.ts
+++ src/recoil/atoms/smite-context-account.ts
@@ -1,15 +1,7 @@
+import { SmitePlayer } from "@/typings/smite/player";
 import { atom } from "recoil";
-
-interface SmiteAccount {
-  Id: string;
-  hz_player_name: string;
-  Team_name: string;
-  Message: string;
-  Platform: string;
-  Last_Login_Datetime: string;
-}
 
 export const smiteAccountState = atom({
   key: "SmiteAccount",
-  default: null as unknown as Partial<SmiteAccount>[] | null
+  default: null as unknown as Partial<SmitePlayer>[] | null
 });
