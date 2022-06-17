ba88b27971dbc9b6fcbd44f1c0df0d751b3bbccf
Wander Souza <wandersouza@live.com>
2022-06-17 19:26:56 -0300

    add twitch user types

---
+++ src/typings/twitch/user.ts
@@ -0,0 +1,13 @@
+export type TwitchUser = {
+  id: string;
+  login: string;
+  display_name: string;
+  type: string;
+  broadcaster_type: string;
+  description: string;
+  profile_image_url: string;
+  offline_image_url: string;
+  view_count: number;
+  email: string;
+  created_at: string;
+};
