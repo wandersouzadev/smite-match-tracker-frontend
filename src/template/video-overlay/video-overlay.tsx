dc581ddfcbcdab8a424ed62d3784855c1895a297
Wander Souza <wandersouza@live.com>
2022-06-17 19:29:39 -0300

    refactor video-overlay template

--- src/template/video-overlay/video-overlay.tsx
+++ src/template/video-overlay/video-overlay.tsx
@@ -1,51 +1,32 @@
-import { MinimizedOverlay, TeamsOverlay } from "@/components/teams-overlay";
-import { useEbs } from "@/hooks/use-ebs";
-import { useTwitchHelper } from "@/hooks/use-twitch-helper";
-import { smiteMatchState } from "@/recoil/atoms/match-state";
-import { minimizedState } from "@/recoil/atoms/system";
-import { twitchAuthDataState } from "@/recoil/atoms/twitch-auth-data";
-import React, { useEffect } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import * as S from "./styles";
+import { SmiteOverlay } from "@/components/smite-overlay";
+import { appConfigState } from "@/recoil/atoms/config-state";
+import React from "react";
+import { useRecoilState } from "recoil";
+import Styles from "./styles.module.scss";
 
 export const OverlayTemplate: React.FC = () => {
-  useTwitchHelper();
-  const twitchAuthData = useRecoilValue(twitchAuthDataState);
-  const setSmiteTeam = useSetRecoilState(smiteMatchState);
-  const minimized = useRecoilValue(minimizedState);
-
-  const { data, isLoading, isError } = useEbs({
-    path: "/smite/live-match",
-    token: twitchAuthData?.token,
-    config: {
-      revalidateOnFocus: true
-    }
-  });
-  useEffect(() => {
-    if (data && data.status === "In Game") {
-      setSmiteTeam({
-        queueId: data.queueId,
-        streamerId: data.accountId,
-        teams: data.teamsData
-      });
-    }
-  }, [data, setSmiteTeam]);
-
-  if (isLoading) {
-    return <template />;
-  }
-
-  if (isError) {
-    return <template />;
-  }
-
-  if (data?.status !== "In Game") {
-    return <template />;
-  }
+  const [appConfig, setAppConfig] = useRecoilState(appConfigState);
 
   return (
-    <S.OverlayWrapper>
-      {minimized ? <MinimizedOverlay /> : <TeamsOverlay />}
-    </S.OverlayWrapper>
+    <div className={Styles.wrapper}>
+      {appConfig.isMinimized ? (
+        <div
+          className={Styles["minimized-overlay"]}
+          onClick={() => setAppConfig({ isMinimized: false })}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex={0}
+        >
+          <img
+            src="overlay-logo.png"
+            alt="logo"
+            className={Styles["overlay-icon"]}
+          />
+          <strong>MATCH TRACKER</strong>
+        </div>
+      ) : (
+        <SmiteOverlay />
+      )}
+    </div>
   );
 };
