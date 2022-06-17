f02e3537c6cf4f3c8c999150f919f4d1582062d9
Wander Souza <wandersouza@live.com>
2022-06-17 19:21:33 -0300

    refactor twitch auth hook

--- src/hooks/use-twitch-auth.ts
+++ src/hooks/use-twitch-auth.ts
@@ -1,15 +1,16 @@
-import { twitchAuthDataState } from "@/recoil/atoms/twitch-auth-data";
+import { twitchAuthState } from "@/recoil/atoms/twitch-auth-data";
 import { twitchHelperState } from "@/recoil/atoms/twitch-helper";
 import { useEffect } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 export const useTwitchAuth = () => {
   const twitchHelper = useRecoilValue(twitchHelperState);
-  const setTwitchAuth = useSetRecoilState(twitchAuthDataState);
+  const [twitchAuth, setTwitchAuth] = useRecoilState(twitchAuthState);
 
   useEffect(() => {
     twitchHelper?.onAuthorized((auth) => {
       setTwitchAuth(auth);
     });
   }, [twitchHelper, setTwitchAuth]);
+  return twitchAuth;
 };
