05e59c1f22ddbefbd8eebdbdd9bfb45014cd2771
Wander Souza <wandersouza@live.com>
2022-06-17 18:09:05 -0300

    add global error component

---
+++ src/components/common/error/error.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Styles from "./styles.module.scss";
+
+export const Error: React.FC = () => {
+  return <div className={Styles.wrapper}>Temporarily unavailable</div>;
+};
