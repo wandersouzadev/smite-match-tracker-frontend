ba7bf1b701b5677f893da3637b8104ed9b9e9774
Wander Souza <wandersouza@live.com>
2022-07-22 21:36:03 -0300

    add unranked conditional tier info

--- src/components/match-tracker/team.tsx
+++ src/components/match-tracker/team.tsx
@@ -2,6 +2,7 @@ import { cdnGodsImgLoad } from "@/helpers/gods-img";
 import { playersCountByGameMode } from "@/helpers/queue";
 import { tierNameById } from "@/helpers/smite-tier";
 import { SmiteMatchPlayer } from "@/typings/smite/match-player";
+import classNames from "classnames";
 import React, { useEffect, useState } from "react";
 import { SpinnerRoundFilled } from "spinners-react";
 import Styles from "./styles.module.scss";
@@ -37,7 +38,7 @@ export const Team: React.FC<Props> = ({
       <tbody>
         {players.map((player) => (
           <tr key={player.GodName}>
-            <td title="God image" className={Styles["content-img"]}>
+            <td title={player.GodName} className={Styles["content-img"]}>
               <img src={cdnGodsImgLoad(player.GodName)} alt={player.GodName} />
             </td>
             <td title="Player name" className={Styles["content-name"]}>
@@ -46,18 +47,19 @@ export const Team: React.FC<Props> = ({
               </p>
               <p>
                 <strong
-                  className={
+                  className={classNames(
                     streamerAccountId === player.playerId
                       ? Styles["streamer-player"]
-                      : {}
-                  }
+                      : {},
+                    player.playerName.length > 13 && Styles.small
+                  )}
                 >
                   {player.playerName}
                 </strong>
               </p>
             </td>
             <td title="MMR" className={Styles["content-mmr"]}>
-              {!!player.Rank_Stat && (
+              {player.Rank_Stat && player.Rank_Stat !== 1500 ? (
                 <>
                   <p>
                     <small>{Math.round(player.Rank_Stat)}</small>
@@ -66,16 +68,29 @@ export const Team: React.FC<Props> = ({
                     <strong>{tierNameById(player.Tier)}</strong>
                   </p>
                 </>
+              ) : (
+                <p>
+                  <strong>Unranked</strong>
+                </p>
               )}
             </td>
             <td title="Wins/Losses" className={Styles["content-winlose"]}>
-              {!!player.Rank_Stat && (
+              {player.Rank_Stat ? (
                 <>
                   <p>
                     <strong>Wins:</strong> {player.tierWins}
                   </p>
                   <p>
                     <strong>Losses:</strong> {player.tierLosses}
+                  </p>
+                </>
+              ) : (
+                <>
+                  <p>
+                    <strong>Wins:</strong> 0
+                  </p>
+                  <p>
+                    <strong>Losses:</strong> 0
                   </p>
                 </>
               )}
@@ -88,7 +103,7 @@ export const Team: React.FC<Props> = ({
             ).map(() => {
               return (
                 <tr key={crypto.randomUUID()}>
-                  <td title="God image" className={Styles["content-img"]}>
+                  <td className={Styles["content-img"]}>
                     <SpinnerRoundFilled color="#ada176" />
                   </td>
 
