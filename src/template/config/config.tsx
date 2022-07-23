63ba45d1761f1a0b6fa1fba023430c29562fe467
Wander Souza <wandersouza@live.com>
2022-07-22 23:55:47 -0300

    remove usecallback

--- src/template/config/config.tsx
+++ src/template/config/config.tsx
@@ -1,50 +1,44 @@
 import { ReactComponent as GithubIcon } from "@/assets/github-logo.svg";
+import { OverlayPosition } from "@/components/overlay-position";
 import { SmiteAccounts } from "@/components/smite-accounts";
 import { SmiteFormAccountConfirmation } from "@/components/smite-form/account-confirmation";
 import { SmiteFormAccountSearch } from "@/components/smite-form/account-search";
 import { SmiteFormPlatformSelect } from "@/components/smite-form/platform-select";
 import { TwitchProfile } from "@/components/twitch-profile";
-import { useTwitchAuth } from "@/hooks/use-twitch-auth";
-import { smiteFormDataState } from "@/recoil/atoms/smite-form-data";
+import { smiteFormState } from "@/recoil/atoms/smite-form";
 import { ArrowClockwise, TwitterLogo } from "phosphor-react";
-import React, { useCallback } from "react";
+import React from "react";
 import { useRecoilValue, useResetRecoilState } from "recoil";
 import Styles from "./styles.module.scss";
 
 export const ConfigTemplate: React.FC = () => {
-  useTwitchAuth();
-  const { step } = useRecoilValue(smiteFormDataState);
-  const resetSmiteFormData = useResetRecoilState(smiteFormDataState);
+  const { step } = useRecoilValue(smiteFormState);
+  const resetSmiteFormData = useResetRecoilState(smiteFormState);
 
   const refreshState = () => {
     resetSmiteFormData();
   };
 
-  const SmiteForm = useCallback(() => {
-    switch (step) {
-      case 1:
-        return <SmiteFormAccountSearch />;
-      case 2:
-        return <SmiteFormAccountConfirmation />;
-      default:
-        return <SmiteFormPlatformSelect />;
-    }
-  }, [step]);
-
   return (
     <div className={Styles["config-wrapper"]}>
       <aside className={Styles.aside}>
-        <div className={Styles["smite-accounts"]}>
-          <h2>My Accounts</h2>
+        <div className={Styles.content}>
+          <h2>Accounts</h2>
           <SmiteAccounts />
         </div>
-        <div className={Styles["twitch-profile"]}>
+        <div className={Styles.content}>
+          <h2>Position</h2>
+          <OverlayPosition />
+        </div>
+        <div className={Styles.content}>
           <TwitchProfile />
         </div>
       </aside>
       <main className={Styles.main}>
         <h1>Smite Match Tracker</h1>
-        <SmiteForm />
+        {step === 0 && <SmiteFormPlatformSelect />}
+        {step === 1 && <SmiteFormAccountSearch />}
+        {step === 2 && <SmiteFormAccountConfirmation />}
       </main>
       <footer className={Styles.footer}>
         <b>Smite Match Tracker v1.0.0</b>
