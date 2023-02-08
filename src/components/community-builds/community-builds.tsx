2375040c2f9b27fed857b1c1d4250efa3d495b30
Wander Souza <wandersouza@live.com>
2023-02-08 15:24:29 -0300

    add community builds component

---
+++ src/components/community-builds/community-builds.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Styles from "./community-builds.modules.scss";
+
+export const CommunityBuilds: React.FC = () => {
+  return (
+    <div className={Styles.wrapper}>
+      <h2>Community Builds</h2>
+      <span>Coming Soon</span>
+    </div>
+  );
+};
---
+++ src/components/community-builds/index.ts
@@ -0,0 +1,1 @@
+export { CommunityBuilds } from "./community-builds";
