51f5ed07f22dfb5efaa85dd838e69617015c7684
Wander Souza <wandersouza@live.com>
2023-02-08 15:39:05 -0300

    add helper to load smite items img from cdn

---
+++ src/helpers/items-image.ts
@@ -0,0 +1,20 @@
+export const itemsImageCdnHelper = (itemName: string) => {
+  let fixedItemName = itemName
+    .toLowerCase()
+    .replace("s8 ", "")
+    .replace("s9 ", "")
+    .replace(" *old*", "")
+    .replace(" (old)", "")
+    .replaceAll(" ", "-")
+    .replaceAll("'", "");
+
+  if (fixedItemName === "cloak-of-meditation") {
+    fixedItemName = "meditation-cloak";
+  }
+
+  if (fixedItemName === "cloak-of-avatar") {
+    fixedItemName = "cloak-of-the-avatar";
+  }
+
+  return `https://webcdn.hirezstudios.com/smite/item-icons/${fixedItemName}.jpg`;
+};
