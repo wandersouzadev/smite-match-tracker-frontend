cb48d5c397e8bd76456b73ac7e70623eb622d4b1
Wander Souza <wandersouza@live.com>
2023-02-08 15:27:50 -0300

    smite method typings get-gods

---
+++ src/typings/smite/get-gods.ts
@@ -0,0 +1,14 @@
+export type GetGods = {
+  Assists: number;
+  Deaths: number;
+  Kills: number;
+  Losses: number;
+  MinionKills: number;
+  Rank: number;
+  Wins: number;
+  Worshippers: number;
+  god: string;
+  god_id: string;
+  player_id: string;
+  ret_msg: any;
+};
