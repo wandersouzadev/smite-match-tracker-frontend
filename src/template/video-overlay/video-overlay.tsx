81c43de2851ec9a6d94cc8baa7d431c4439f4136
Wander Souza <wandersouza@live.com>
2023-02-08 17:04:36 -0300

    move overlay open/close logic to overlay template

--- src/template/video-overlay/video-overlay.tsx
+++ src/template/video-overlay/video-overlay.tsx
@@ -4,7 +4,9 @@ import { useEbs } from "@/hooks/use-ebs";
 import { useTwitchAuth } from "@/hooks/use-twitch-auth";
 import { useTwitchContext } from "@/hooks/use-twitch-context";
 import { appConfigState } from "@/recoil/atoms/app-config";
-import React, { useEffect } from "react";
+import cx from "classnames";
+import { CaretLeft, CaretRight } from "phosphor-react";
+import React, { useEffect, useMemo } from "react";
 import { useRecoilState } from "recoil";
 import Styles from "./video-overlay.module.scss";
 
@@ -31,13 +33,49 @@ export const OverlayTemplate: React.FC = () => {
     });
   }, [data, setAppConfig]);
 
+  const OpenCloseButtonIconToggle = useMemo(() => {
+    if (appConfig.position.includes("left")) {
+      return appConfig.isMinimized ? <CaretRight /> : <CaretLeft />;
+    }
+    return appConfig.isMinimized ? <CaretLeft /> : <CaretRight />;
+  }, [appConfig.position, appConfig.isMinimized]);
+
   if (twitchContext?.game !== "SMITE") {
     return <template />;
   }
 
   return (
-    <div className={Styles.wrapper}>
+    <div
+      className={cx(
+        Styles.wrapper,
+        appConfig?.position ? Styles[appConfig?.position] : Styles.left,
+        appConfig?.isMinimized === false &&
+          (appConfig?.position === "left" || appConfig?.position === "right") &&
+          Styles["middle-maximized"],
+        appConfig?.isMinimized === false &&
+          appConfig?.position.includes("left") &&
+          Styles["left-maximized"],
+        appConfig?.isMinimized === false &&
+          appConfig?.position.includes("right") &&
+          Styles["right-maximized"],
+        appConfig?.position.includes("left") && Styles["left-side"],
+        appConfig?.position.includes("right") && Styles["right-side"]
+      )}
+    >
       {appConfig?.isMinimized ? <SmiteOverlayMinimized /> : <SmiteOverlay />}
+      <div className={cx(Styles["open-close-toggle"])}>
+        <button
+          type="button"
+          onClick={() =>
+            setAppConfig((oldValue) => ({
+              ...oldValue,
+              isMinimized: !oldValue.isMinimized
+            }))
+          }
+        >
+          {OpenCloseButtonIconToggle}
+        </button>
+      </div>
     </div>
   );
 };
