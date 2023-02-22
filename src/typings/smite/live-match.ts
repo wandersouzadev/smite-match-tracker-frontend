27a583bc0b01ed9a1301d9f20fda68d9c9dc67b4
Wander Souza <wandersouza@live.com>
2023-02-21 22:19:56 -0300

    add "Unknown" SMITE account status

--- src/typings/smite/live-match.ts
+++ src/typings/smite/live-match.ts
@@ -1,8 +1,9 @@
 import { SmiteMatchPlayer } from "./match-player";
 
 export interface SmiteLiveMatch {
+  accountName: string;
   accountId: string;
   queueId: string;
-  status: "Offline" | "In Lobby" | "God Selection" | "In Game";
+  status: "Offline" | "In Lobby" | "God Selection" | "In Game" | "Unknown";
   teamsData: SmiteMatchPlayer[];
 }
