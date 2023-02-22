2371e6d7de616b15745b7b0727bd1dd21da01107
Wander Souza <wandersouza@live.com>
2023-02-21 22:06:22 -0300

    add mmr to tier helper

---
+++ src/helpers/mmr-to-tier.ts
@@ -0,0 +1,21 @@
+export const mmrToTierHelper = (mmr: number | string | null) => {
+  if (!mmr) {
+    return "";
+  }
+  switch (true) {
+    case mmr < 639:
+      return "BRONZE";
+    case mmr >= 640 && mmr < 1204:
+      return "Silver";
+    case mmr >= 1205 && mmr < 1524:
+      return "Gold";
+    case mmr >= 1525 && mmr < 1999:
+      return "Platinum";
+    case mmr >= 2000 && mmr < 2499:
+      return "Diamond";
+    case mmr >= 2500:
+      return "Master";
+    default:
+      return "";
+  }
+};
