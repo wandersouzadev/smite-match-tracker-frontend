8559c27825c177af8ee76164aa9e493bd5fe5244
Wander Souza <wandersouza@live.com>
2023-02-24 18:36:27 -0300

    fix: calc miss value

--- src/helpers/mmr-to-tier.ts
+++ src/helpers/mmr-to-tier.ts
@@ -3,15 +3,15 @@ export const mmrToTierHelper = (mmr: number | string | null) => {
     return "";
   }
   switch (true) {
-    case mmr < 639:
+    case mmr <= 639:
       return "BRONZE";
-    case mmr >= 640 && mmr < 1204:
+    case mmr >= 640 && mmr <= 1204:
       return "Silver";
-    case mmr >= 1205 && mmr < 1524:
+    case mmr >= 1205 && mmr <= 1524:
       return "Gold";
-    case mmr >= 1525 && mmr < 1999:
+    case mmr >= 1525 && mmr <= 1999:
       return "Platinum";
-    case mmr >= 2000 && mmr < 2499:
+    case mmr >= 2000 && mmr <= 2499:
       return "Diamond";
     case mmr >= 2500:
       return "Master";
